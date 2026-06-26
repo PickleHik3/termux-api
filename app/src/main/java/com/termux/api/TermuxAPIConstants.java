f33d928891639bf10d38bd51d0a506935fe187bf
PickleHik3 <vajdan.amal@live.com>
2026-06-26 20:15:38 +0300

    Phase 0B: adopt VAJ Terminal API identity

--- app/src/main/java/com/termux/api/TermuxAPIConstants.java
+++ app/src/main/java/com/termux/api/TermuxAPIConstants.java
@@ -9,7 +9,7 @@ public class TermuxAPIConstants {
     /**
      * Termux:API Receiver name.
      */
-    public static final String TERMUX_API_RECEIVER_NAME = TERMUX_API_PACKAGE_NAME + ".TermuxApiReceiver"; // Default to "com.termux.api.TermuxApiReceiver"
+    public static final String TERMUX_API_RECEIVER_NAME = "com.termux.api.TermuxApiReceiver";
 
     /** The Uri authority for Termux:API app file shares */
     public static final String TERMUX_API_FILE_SHARE_URI_AUTHORITY = TERMUX_PACKAGE_NAME + ".sharedfiles"; // Default: "com.termux.sharedfiles"
